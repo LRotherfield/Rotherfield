0da05fdedc1b1d4a06c69e023191db877ab2dd44
Luke Rotherfield <l.rotherfield@gmail.com>
2012-06-05 21:15:06 +0100

    show page

--- src/Rothers/PortfolioBundle/Controller/FrontendController.php
+++ src/Rothers/PortfolioBundle/Controller/FrontendController.php
@@ -6,10 +6,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
+/**
+ * @Route("/")
+ */
 class FrontendController extends Controller
 {
+
     /**
-     * @Route("/")
+     * @Route("", name="portfolio_index")
      * @Template()
      */
     public function indexAction()
@@ -18,4 +22,16 @@ class FrontendController extends Controller
         $entities = $em->getRepository("PortfolioBundle:Portfolio")->findAll();
         return array('entities' => $entities);
     }
+
+    /**
+     * @Route("portfolio/{name}", name="portfolio_show")
+     * @Template()
+     */
+    public function showAction($name)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+        $portfolio = $em->getRepository("PortfolioBundle:Portfolio")->findOneBy(array("name" => $name));
+        return array('portfolio' => $portfolio);
+    }
+
 }
